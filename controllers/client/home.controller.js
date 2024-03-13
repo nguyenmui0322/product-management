6ac4884ad140737deca9935a462d9a5cb71db0b5
mui <nvanmui8@gmail.com>
2024-03-13 22:34:30 +0700

    show featured products

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,19 @@
+const Product = require("../../models/product.model");
+
+const productsHelper = require("../../helpers/products");
+
 // [GET] /
 module.exports.index = async (req, res) => {
+  const productsFeatured = await Product.find({
+    featured: "1",
+    deleted: false,
+    status: "active",
+  }).limit(3);
+
+  const newProducts = productsHelper.priceNewProducts(productsFeatured);
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
+    productsFeatured: newProducts,
   });
 };
