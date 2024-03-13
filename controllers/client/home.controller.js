3574b59c78f0b11b2ffa4af599b82908d19d22b5
mui <nvanmui8@gmail.com>
2024-03-13 23:20:09 +0700

    show new products

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -9,11 +9,19 @@ module.exports.index = async (req, res) => {
     deleted: false,
     status: "active",
   }).limit(3);
+  const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured);
 
-  const newProducts = productsHelper.priceNewProducts(productsFeatured);
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active",
+  })
+    .sort({ position: "desc" })
+    .limit(6);
+  const newProductsNew = productsHelper.priceNewProducts(productsNew);
 
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    productsFeatured: newProducts,
+    productsFeatured: newProductsFeatured,
+    productsNew: newProductsNew,
   });
 };
