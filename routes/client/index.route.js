26acde78a00c3846a8fb81934155ec1b70916a71
mui <nvanmui8@gmail.com>
2024-03-18 21:56:00 +0700

    order success

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -5,6 +5,7 @@ const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
 const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
+const checkoutRoutes = require("./checkout.route");
 
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
@@ -18,4 +19,6 @@ module.exports = (app) => {
   app.use("/search", searchRoutes);
 
   app.use("/cart", cartRoutes);
+
+  app.use("/checkout", checkoutRoutes);
 };
