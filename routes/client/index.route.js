a5b22574a55db8a049ff5431312155ff1be02a3d
mui <nvanmui8@gmail.com>
2024-03-19 14:44:01 +0700

    register (client)

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -6,6 +6,7 @@ const productRoutes = require("./product.route");
 const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
+const userRoutes = require("./user.route");
 
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
@@ -21,4 +22,6 @@ module.exports = (app) => {
   app.use("/cart", cartRoutes);
 
   app.use("/checkout", checkoutRoutes);
+
+  app.use("/user", userRoutes);
 };
