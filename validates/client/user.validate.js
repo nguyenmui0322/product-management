efd0fb1885e1fec4cb85960397d50486e27c8232
mui <nvanmui8@gmail.com>
2024-03-19 17:53:09 +0700

    update forgot password

--- validates/client/user.validate.js
+++ validates/client/user.validate.js
@@ -30,3 +30,13 @@ module.exports.loginPost = (req, res, next) => {
   }
   next();
 };
+
+module.exports.forgotPasswordPost = (req, res, next) => {
+  if (!req.body.email) {
+    req.flash("error", `Vui lòng nhập email!`);
+    res.redirect("back");
+    return;
+  }
+
+  next();
+};
