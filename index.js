77d846dcf7c34b1bc3db751e6dcc36b8ac1a89be
mui <nvanmui8@gmail.com>
2024-03-21 00:14:51 +0700

    404

--- index.js
+++ index.js
@@ -49,6 +49,11 @@ app.use(express.static(`${__dirname}/public`));
 // Route
 routeAdmin(app);
 route(app);
+app.get("*", (req, res) => {
+  res.render("client/pages/errors/404", {
+    pageTitle: "404 Not Found",
+  });
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
